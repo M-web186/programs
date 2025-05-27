f31f0fa9dfeb9adf7b973b5f76700fd58e6bb4d5
M-web186 <maureenwyse667@gmail.com>
2025-05-27 09:28:24 +0300

    Create web.cpp

---
+++ web.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main()
+int n=4;
+cout<<"n:";
+scanf("%d",&n);
+for (int i=1; i<=n; i++)
+{
+for(int j=1; j<=i; j++)
+{
+cout<<"*";
+}
+cout<<"\n";
+}
+return 0;
+}
